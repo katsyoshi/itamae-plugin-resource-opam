a6b0d8acffdd926a86e84621af1c91cb1167e611
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-09-12 23:28:46 +0900

    initial commit

---
+++ lib/itamae/plugin/resource/opam.rb
@@ -0,0 +1,11 @@
+require "itamae/plugin/resource/opam/version"
+
+module Itamae
+  module Plugin
+    module Resource
+      module Opam
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/itamae/plugin/resource/opam/version.rb
@@ -0,0 +1,9 @@
+module Itamae
+  module Plugin
+    module Resource
+      module Opam
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
