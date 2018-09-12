5e428aea8090c30c55a11b92e9321c4cc48513ef
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-09-13 01:42:42 +0900

    remove unnecessary options

--- lib/itamae/plugin/resource/opam.rb
+++ lib/itamae/plugin/resource/opam.rb
@@ -7,7 +7,6 @@ module Itamae
         define_attribute :action, default: :install
         define_attribute :opam_binary, type: [String, Array], default: 'opam'
         define_attribute :package_name, type: String, default_name: true
-        define_attribute :options, type: [String, Array], default: :auto
         define_attribute :version, type: String, default: false
 
         def pre_action
