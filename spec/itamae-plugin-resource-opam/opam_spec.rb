089bcf64633cb1dbcf64b7aa9fc245b85168b202
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2018-09-19 16:06:00 +0900

    check exist and executable file

--- spec/itamae-plugin-resource-opam/opam_spec.rb
+++ spec/itamae-plugin-resource-opam/opam_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 
-describe file("/root/.opam/4.05.0/bin/coqc") do
-  it { should be_directory }
+describe file("/root/.opam/4.06.0/bin/coqc") do
+  it { should be_file }
+  it { should be_executable }
 end
